354f2cbc63154121b109518151e7f9c68dbadfe3
Robert Black <rbla4655@gmail.com>
2016-05-02 18:33:28 -0400

    initial

---
+++ app/data/friends.js
@@ -0,0 +1,5 @@
+var friends = [
+{name: "Johnny Depp", gender: "Male", scores: [1,4,5,3,3,1,2,3,1,5], pic: "http://ia.media-imdb.com/images/M/MV5BMTM0ODU5Nzk2OV5BMl5BanBnXkFtZTcwMzI2ODgyNQ@@._V1_UY1200_CR106,0,630,1200_AL_.jpg"}
+];
+
+module.exports = friends;
